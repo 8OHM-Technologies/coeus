3c7ec967efa2d6d469d2e86b5ea24493b65d6259
Tiaan Fourie <tiaanf@8ohm.co.za>
2026-06-28 14:17:32 +0200

    chore: improve Telegram deployment notifications formatting and update django-daisy dependency

--- control_plane/core/settings.py
+++ control_plane/core/settings.py
@@ -43,7 +43,7 @@ CSRF_TRUSTED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'daisyui_dashboard',
+    "daisyui_dashboard",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
