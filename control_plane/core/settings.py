ed425f78bbcd2cd2b5011d636ace987d9dd1812a
Tiaan Fourie <tiaanf@8ohm.co.za>
2026-06-28 15:29:34 +0200

    chore: remove django-daisy dependency and unregister from installed apps

--- control_plane/core/settings.py
+++ control_plane/core/settings.py
@@ -43,7 +43,6 @@ CSRF_TRUSTED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
-    "django_daisy",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
