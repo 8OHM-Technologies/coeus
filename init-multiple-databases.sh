625fda994731ec63934f0999977fe63733a41a16
TiaanF <tiaanf@8ohm.co.za>
2026-07-04 16:32:14 +0200

    refactor: update multi-database initialization script to check for existing databases before creation

--- init-multiple-databases.sh
+++ init-multiple-databases.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 # init-multiple-databases.sh
 set -e
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' '\n' | xargs -I {} echo "CREATE DATABASE \"{}\";")
+# Split comma-separated string into an array
+IFS=',' read -ra DB_ARRAY <<< "$POSTGRES_MULTIPLE_DATABASES"
+
+for db in "${DB_ARRAY[@]}"; do
+    # Remove leading/trailing whitespaces
+    db=$(echo "$db" | xargs)
+    
+    # Generate CREATE DATABASE query only if it does not exist, then pipe to \gexec
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+        SELECT 'CREATE DATABASE "$db"' 
+        WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$db') 
+        \gexec
 EOSQL
+done
